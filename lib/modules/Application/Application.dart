1a63ab24bf2883b0dc70c8c16ce7a5050f3c5d8f
牛新怀 <niuxinhuai@fanfandata.com>
2019-08-09 16:54:07 +0800

    fix applicaiton

--- lib/modules/Application/Application.dart
+++ lib/modules/Application/Application.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:weibo_flutter/const/apiConst.dart';
+import 'package:weibo_flutter/const/sp_helper.dart';
+import 'package:weibo_flutter/const/userDefault.dart';
 import 'package:weibo_flutter/modules/Application/model.dart';
+import 'package:weibo_flutter/modules/web/oauth.dart';
+import 'package:weibo_flutter/service/service_manager.dart';
 
 class Applicaiton extends StatefulWidget {
   @override
@@ -9,31 +14,47 @@ class Applicaiton extends StatefulWidget {
 class _ApplicaitonState extends State<Applicaiton>
     with SingleTickerProviderStateMixin {
   PageController _pageController;
+  SpHelper sp;
   int _curIndex = 0;
-
+  bool go = false;
+  final String oauthUrl = API.getOauthUrl();
   @override
   void initState() {
     super.initState();
     _pageController = PageController();
+    getTokenState();
+  }
+
+  getTokenState() async {
+    sp = await SpHelper.getInstance();
+    setState(() {
+      go = sp.showWBoo();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: new PageView(
-        physics: NeverScrollableScrollPhysics(),
-        children: ApplicationModel.tabControllers,
-        controller: _pageController,
-        onPageChanged: onPageChanged,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: ApplicationModel.items,
-        currentIndex: _curIndex,
-        type: BottomNavigationBarType.fixed,
-        fixedColor: Colors.black,
-        onTap: onTap,
-      ),
-    );
+    return go
+        ? Scaffold(
+            body: new PageView(
+              physics: NeverScrollableScrollPhysics(),
+              children: ApplicationModel.tabControllers,
+              controller: _pageController,
+              onPageChanged: onPageChanged,
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              items: ApplicationModel.items,
+              currentIndex: _curIndex,
+              type: BottomNavigationBarType.fixed,
+              fixedColor: Colors.black,
+              onTap: onTap,
+            ),
+          )
+        : WebPage(
+            url: oauthUrl,
+            title: '微博登录',
+            valueChanged: _back,
+          );
   }
 
   void onTap(int tag) {
@@ -44,6 +65,21 @@ class _ApplicaitonState extends State<Applicaiton>
   void onPageChanged(int page) {
     setState(() {
       this._curIndex = page;
+    });
+  }
+
+  //* 授权code回调 */
+  _back(String code) {
+    ServiceManager.getAccessToken(code).then((models) {
+      SpHelper.setString(models.accessToken, UserDefaults.TOKEN_KEY).then((b) {
+        if (b) {
+          setState(() {
+            go = true;
+          });
+        }
+      });
+      SpHelper.setString(
+          (models.expires * 10).toString(), UserDefaults.TOKEN_TIME_KEY);
     });
   }
 
