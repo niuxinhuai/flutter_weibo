5a266d5ffb17a328ced2f6f45b566dab5bedb26d
牛新怀 <niuxinhuai@fanfandata.com>
2019-08-09 16:51:47 +0800

    add webOauth

---
+++ lib/modules/web/oauth.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:weibo_flutter/widget/appbar.dart';
+
+typedef ValueChanged(String codeStr);
+
+class WebPage extends StatefulWidget {
+  @required
+  final String url;
+  final String title;
+  @required
+  ValueChanged valueChanged;
+  WebPage({this.url, this.title, this.valueChanged});
+
+  @override
+  _WebPageState createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  FlutterWebviewPlugin plugin;
+  final String code = 'code=';
+  @override
+  void initState() {
+    super.initState();
+    plugin = FlutterWebviewPlugin();
+    plugin.onStateChanged.listen((state) {
+      if (state.type == WebViewState.finishLoad) {
+        print('>>>>>加载完成');
+      } else if (state.type == WebViewState.shouldStart) {
+        print('>>>>>>准备加载');
+      } else if (state.type == WebViewState.startLoad) {
+        print('>>>>>>>开始加载');
+      }
+    });
+    plugin.onUrlChanged.listen((value) {
+      print('>>>>>>>>改变的url是${value}');
+      if (value.contains(code)) {
+        final codeString = value.split(code).last;
+        widget.valueChanged(codeString);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: widget.url,
+      appBar: FWAppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
