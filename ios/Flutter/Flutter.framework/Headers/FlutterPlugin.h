eb0d1d46cee6645006df02a5587880d4fdb91d82
牛新怀 <niuxinhuai@fanfandata.com>
2019-09-28 16:14:35 +0800

    fix other

--- ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h
+++ ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h
@@ -120,11 +120,11 @@ typedef void (*FlutterPluginRegistrantCallback)(NSObject<FlutterPluginRegistry>*
 /**
  * Called if this plugin has been registered for `UIApplicationDelegate` callbacks.
  */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 - (void)application:(UIApplication*)application
-    didRegisterUserNotificationSettings:(UIUserNotificationSettings*)notificationSettings
-    API_DEPRECATED(
-        "See -[UIApplicationDelegate application:didRegisterUserNotificationSettings:] deprecation",
-        ios(8.0, 10.0));
+    didRegisterUserNotificationSettings:(UIUserNotificationSettings*)notificationSettings;
+#pragma GCC diagnostic pop
 
 /**
  * Called if this plugin has been registered for `UIApplicationDelegate` callbacks.
@@ -145,10 +145,7 @@ typedef void (*FlutterPluginRegistrantCallback)(NSObject<FlutterPluginRegistry>*
  * Calls all plugins registered for `UIApplicationDelegate` callbacks.
  */
 - (void)application:(UIApplication*)application
-    didReceiveLocalNotification:(UILocalNotification*)notification
-    API_DEPRECATED(
-        "See -[UIApplicationDelegate application:didReceiveLocalNotification:] deprecation",
-        ios(4.0, 10.0));
+    didReceiveLocalNotification:(UILocalNotification*)notification;
 
 /**
  * Calls all plugins registered for `UNUserNotificationCenterDelegate` callbacks.
@@ -249,7 +246,7 @@ typedef void (*FlutterPluginRegistrantCallback)(NSObject<FlutterPluginRegistry>*
 - (NSObject<FlutterTextureRegistry>*)textures;
 
 /**
- * Registers a `FlutterPlatformViewFactory` for creation of platform views.
+ * Registers a `FlutterPlatformViewFactory` for creation of platfrom views.
  *
  * Plugins expose `UIView` for embedding in Flutter apps by registering a view factory.
  *
